b4fc8f8d5c45c0d82ac84fade5f45b64435a7bb8
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-24 22:50:35 +0530

    Created a Rule Action entity and its repository

---
+++ src/main/java/com/example/ruleEngine/entity/RuleAction.java
@@ -0,0 +1,26 @@
+package com.example.ruleEngine.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RuleAction {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "rule_id")
+    private Rule rule;
+
+    private String outputField;
+    private String outputValue;
+}
