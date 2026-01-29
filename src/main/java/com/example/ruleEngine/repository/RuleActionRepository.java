e68cda5254701daa1fc9cbd68ee7e5d467b5d10b
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-29 23:09:27 +0530

    Added needed repository methods

--- src/main/java/com/example/ruleEngine/repository/RuleActionRepository.java
+++ src/main/java/com/example/ruleEngine/repository/RuleActionRepository.java
@@ -3,5 +3,8 @@ package com.example.ruleEngine.repository;
 import com.example.ruleEngine.entity.RuleAction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RuleActionRepository extends JpaRepository<RuleAction, Long> {
+    List<RuleAction> findByRuleId(Long id);
 }
--- src/main/java/com/example/ruleEngine/repository/RuleConditionRepository.java
+++ src/main/java/com/example/ruleEngine/repository/RuleConditionRepository.java
@@ -3,5 +3,8 @@ package com.example.ruleEngine.repository;
 import com.example.ruleEngine.entity.RuleCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RuleConditionRepository extends JpaRepository<RuleCondition, Long> {
+    List<RuleCondition> findByRuleId(Long id);
 }
